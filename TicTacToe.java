6c0e7fe14869a9283a7dbf14d0dfd93a7776b399
Jose D. Romero <josedanilromero@gmail.com>
2024-05-31 11:19:20 -0500

    Commit 12: set textField background color, foreground color (i.e. the color of the letters in the text field) and font size and type

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,11 +14,16 @@ class TicTacToe implements ActionListener{
     JButton[] buttons = new JButton[9];
     boolean player1_turn;
 
+    // Constructor
     TicTacToe(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         frame.getContentPane().setBackground(new Color(50,50,50));
         frame.setLayout(new BorderLayout());
+
+        textField.setBackground(new Color(25, 25, 25));
+        textField.setForeground(new Color(25, 255, 0));
+        textField.setFont(new Font("Noteworthy", Font.BOLD,75));
     }
 
     @Override
