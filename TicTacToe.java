f3c52a36ba447b3c70e593a052542e4ab8f9aa27
Jose D. Romero <josedanilromero@gmail.com>
2024-06-09 13:57:38 -0500

    feat(TicTacToe): Add check() method body
    The check() method can be used to check for wins that happen either in rows, columns, or diagonally either for player X or player O

--- TicTacToe.java
+++ TicTacToe.java
@@ -103,7 +103,124 @@ public class TicTacToe implements ActionListener{
 
     // check() method checks when there is a winner
     public void check(){
-
+        // Row X Wins
+        if(
+            (buttons[0].getText() == "X") &&
+            (buttons[1].getText() == "X") &&
+            (buttons[2].getText() == "X")
+        ) {
+            xWins(0, 1, 2);
+        }
+        if(
+            (buttons[3].getText() == "X") &&
+            (buttons[4].getText() == "X") &&
+            (buttons[5].getText() == "X")
+        ) {
+            xWins(3, 4, 5);
+        }
+        if(
+            (buttons[6].getText() == "X") &&
+            (buttons[7].getText() == "X") &&
+            (buttons[8].getText() == "X")
+        ) {
+            xWins(6, 7, 8);
+        }
+        // Column X Wins
+        if(
+            (buttons[0].getText() == "X") &&
+            (buttons[3].getText() == "X") &&
+            (buttons[6].getText() == "X")
+        ) {
+            xWins(0, 3, 6);
+        }
+        if(
+            (buttons[1].getText() == "X") &&
+            (buttons[4].getText() == "X") &&
+            (buttons[7].getText() == "X")
+        ) {
+            xWins(1, 4, 7);
+        }
+        if(
+            (buttons[2].getText() == "X") &&
+            (buttons[5].getText() == "X") &&
+            (buttons[8].getText() == "X")
+        ) {
+            xWins(2, 5, 8);
+        }
+        // Diagonal X Wins
+        if(
+            (buttons[0].getText() == "X") &&
+            (buttons[4].getText() == "X") &&
+            (buttons[8].getText() == "X")
+        ) {
+            xWins(0, 4, 8);
+        }
+        if(
+            (buttons[2].getText() == "X") &&
+            (buttons[4].getText() == "X") &&
+            (buttons[6].getText() == "X")
+        ) {
+            xWins(2, 4, 6);
+        }
+        // Row O Wins
+        if(
+            (buttons[0].getText() == "O") &&
+            (buttons[1].getText() == "O") &&
+            (buttons[2].getText() == "O")
+        ) {
+            oWins(0, 1, 2);
+        }
+        if(
+            (buttons[3].getText() == "O") &&
+            (buttons[4].getText() == "O") &&
+            (buttons[5].getText() == "O")
+        ) {
+            oWins(3, 4, 5);
+        }
+        if(
+            (buttons[6].getText() == "O") &&
+            (buttons[7].getText() == "O") &&
+            (buttons[8].getText() == "O")
+        ) {
+            oWins(6, 7, 8);
+        }
+        // Column O Wins
+        if(
+            (buttons[0].getText() == "O") &&
+            (buttons[3].getText() == "O") &&
+            (buttons[6].getText() == "O")
+        ) {
+            oWins(0, 3, 6);
+        }
+        if(
+            (buttons[1].getText() == "O") &&
+            (buttons[4].getText() == "O") &&
+            (buttons[7].getText() == "O")
+        ) {
+            oWins(1, 4, 7);
+        }
+        if(
+            (buttons[2].getText() == "O") &&
+            (buttons[5].getText() == "O") &&
+            (buttons[8].getText() == "O")
+        ) {
+            oWins(2, 5, 8);
+        }
+        // Diagonal O Wins
+        if(
+            (buttons[0].getText() == "O") &&
+            (buttons[4].getText() == "O") &&
+            (buttons[8].getText() == "O")
+        ) {
+            oWins(0, 4, 8);
+        }
+        if(
+            (buttons[2].getText() == "O") &&
+            (buttons[4].getText() == "O") &&
+            (buttons[6].getText() == "O")
+        ) {
+            oWins(2, 4, 6);
+        }
     }
 
     // xWins() method will highlight which spots x won in
