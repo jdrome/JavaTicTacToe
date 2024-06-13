aca633da98b6c83068da2b3b566e2ef57b9e6e10
Jose D. Romero <josedanilromero@gmail.com>
2024-06-13 13:31:17 -0500

    feat(TicTacToe): Add resetButton functionality via new resetGame() method

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        new TicTacToeFrame();
+        TicTacToe tictactoe = new TicTacToe();
     }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -3,20 +3,36 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-import javax.swing.JPanel;
 
-public class TicTacToe extends JPanel implements ActionListener{
+public class TicTacToe implements ActionListener{
 
     // Class Attributes
     Random random = new Random();
+    JFrame frame = new JFrame(); // This JFrame will hold our game board
     JPanel titlePanel = new JPanel();
     JPanel buttonPanel = new JPanel();
+    JPanel resetButtonPanel = new JPanel();
     JLabel textField = new JLabel();
     JButton[] buttons = new JButton[9];
+    JButton resetButton = new JButton();
     boolean player1Turn;
 
     // Constructor
     TicTacToe(){
+        // Change UI look and feel to match cross-platform look and feel
+        // This will allow us to change the look and feel of JButtons and have them 
+        // match the look and feel of the rest of the OS.
+        try{
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+          }catch(Exception e){
+           e.printStackTrace(); 
+          }
+        
+        // Defines scale and style of frame JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
 
         // Defines scale and style of textField JLabel - this is essentially the title of the gameboard
         textField.setBackground(new Color(25, 25, 25));
@@ -34,15 +50,38 @@ public class TicTacToe extends JPanel implements ActionListener{
         buttonPanel.setLayout(new GridLayout(3, 3));
         buttonPanel.setBackground(new Color (150, 150, 150));
 
-        // Create and add X/O buttons to buttonPanel
+        // Location and layout for resetButtonPanel
+        resetButtonPanel.setLayout(new BorderLayout());
+        resetButtonPanel.setBounds(0, 600, 800, 100);
+
+        // Configuration of resetButton
+        resetButton = new JButton();
+        resetButtonPanel.add(resetButton);
+        resetButton.setFont(new Font("Noteworthy", Font.BOLD, 75));
+        resetButton.setForeground(new Color(255, 255, 255));
+        resetButton.setBackground(Color.RED);
+        resetButton.setOpaque(true);
+        resetButton.setText("Reset");
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(this);
+
+        // Create and add buttons JButton to buttonPanel
         for(int i = 0; i < 9; i++){
             buttons[i] = new JButton();
             buttonPanel.add(buttons[i]);
             buttons[i].setFont(new Font("Noteworthy", Font.BOLD,120));
-            buttons[i].setBackground(new Color(25, 25, 25));
             buttons[i].setFocusable(false);
             buttons[i].addActionListener(this);
         }
+
+        // Add Components to Panel and Frame
+        titlePanel.add(textField);
+        frame.add(titlePanel, BorderLayout.NORTH);
+        frame.add(buttonPanel);
+        resetButtonPanel.add(resetButton);
+        frame.add(resetButtonPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true); // This allows us to see the board when running the code.
 
         firstTurn();
     } // End of constructor
@@ -70,6 +109,9 @@ public class TicTacToe extends JPanel implements ActionListener{
                     }
                 }
             }
+        }
+        if (e.getSource() == resetButton) {
+            resetGame();
         }
     }
 
@@ -254,5 +296,15 @@ public class TicTacToe extends JPanel implements ActionListener{
         }
 
         textField.setText("O Wins!");
+    }
+    
+    public void resetGame() {
+        for (int i = 0; i < 9; i++) {
+            buttons[i].setText("");
+            buttons[i].setEnabled(true);
+            buttons[i].setBackground(new JButton().getBackground());
+        }
+        textField.setText("Tic-Tac-Toe");
+        firstTurn();
     }
 }
