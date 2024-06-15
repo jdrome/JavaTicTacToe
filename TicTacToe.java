bc5051ca1fe378391cdd64a45e0381ab39dd8788
Jose D. Romero <josedanilromero@gmail.com>
2024-06-15 12:21:14 -0500

    fix(TicTacToe): Fix tie feature
    The logic of the tie feature would trigger when all spots are marked, but it should only trigger if a winner was not already declared. Sometimes there would be a winner but the winning condition was met by also selecting a spot that would fill up the board, triggering the textField to say tie when it should have declared a winner. To fix this, I changed the conditions that check for a winner from being all if statements to also being if else statements

--- TicTacToe.java
+++ TicTacToe.java
@@ -150,14 +150,14 @@ public class TicTacToe implements ActionListener{
         ) {
             xWins(0, 1, 2);
         }
-        if(
+        else if(
             (buttons[3].getText() == "X") &&
             (buttons[4].getText() == "X") &&
             (buttons[5].getText() == "X")
         ) {
             xWins(3, 4, 5);
         }
-        if(
+        else if(
             (buttons[6].getText() == "X") &&
             (buttons[7].getText() == "X") &&
             (buttons[8].getText() == "X")
@@ -166,21 +166,21 @@ public class TicTacToe implements ActionListener{
         }
         
         // Column X Wins
-        if(
+        else if(
             (buttons[0].getText() == "X") &&
             (buttons[3].getText() == "X") &&
             (buttons[6].getText() == "X")
         ) {
             xWins(0, 3, 6);
         }
-        if(
+        else if(
             (buttons[1].getText() == "X") &&
             (buttons[4].getText() == "X") &&
             (buttons[7].getText() == "X")
         ) {
             xWins(1, 4, 7);
         }
-        if(
+        else if(
             (buttons[2].getText() == "X") &&
             (buttons[5].getText() == "X") &&
             (buttons[8].getText() == "X")
@@ -189,14 +189,14 @@ public class TicTacToe implements ActionListener{
         }
 
         // Diagonal X Wins
-        if(
+        else if(
             (buttons[0].getText() == "X") &&
             (buttons[4].getText() == "X") &&
             (buttons[8].getText() == "X")
         ) {
             xWins(0, 4, 8);
         }
-        if(
+        else if(
             (buttons[2].getText() == "X") &&
             (buttons[4].getText() == "X") &&
             (buttons[6].getText() == "X")
@@ -207,21 +207,21 @@ public class TicTacToe implements ActionListener{
         // Check for O Winning Conditions
 
         // Row O Wins
-        if(
+        else if(
             (buttons[0].getText() == "O") &&
             (buttons[1].getText() == "O") &&
             (buttons[2].getText() == "O")
         ) {
             oWins(0, 1, 2);
         }
-        if(
+        else if(
             (buttons[3].getText() == "O") &&
             (buttons[4].getText() == "O") &&
             (buttons[5].getText() == "O")
         ) {
             oWins(3, 4, 5);
         }
-        if(
+        else if(
             (buttons[6].getText() == "O") &&
             (buttons[7].getText() == "O") &&
             (buttons[8].getText() == "O")
@@ -230,21 +230,21 @@ public class TicTacToe implements ActionListener{
         }
 
         // Column O Wins
-        if(
+        else if(
             (buttons[0].getText() == "O") &&
             (buttons[3].getText() == "O") &&
             (buttons[6].getText() == "O")
         ) {
             oWins(0, 3, 6);
         }
-        if(
+        else if(
             (buttons[1].getText() == "O") &&
             (buttons[4].getText() == "O") &&
             (buttons[7].getText() == "O")
         ) {
             oWins(1, 4, 7);
         }
-        if(
+        else if(
             (buttons[2].getText() == "O") &&
             (buttons[5].getText() == "O") &&
             (buttons[8].getText() == "O")
@@ -253,14 +253,14 @@ public class TicTacToe implements ActionListener{
         }
 
         // Diagonal O Wins
-        if(
+        else if(
             (buttons[0].getText() == "O") &&
             (buttons[4].getText() == "O") &&
             (buttons[8].getText() == "O")
         ) {
             oWins(0, 4, 8);
         }
-        if(
+        else if(
             (buttons[2].getText() == "O") &&
             (buttons[4].getText() == "O") &&
             (buttons[6].getText() == "O")
@@ -269,7 +269,7 @@ public class TicTacToe implements ActionListener{
         }
 
         // Draw - no winner
-        if(
+        else if(
             (buttons[0].getText() != "") &&
             (buttons[1].getText() != "") &&
             (buttons[2].getText() != "") &&
